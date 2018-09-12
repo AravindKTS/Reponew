abe8ab30fed917a2730834c64ae40531db5c49bf
Aravind <Tsaitprofessional@gmail.com>
2018-09-12 10:48:43 +0530

    Initial Version of Party.py
    Repo party.py

---
+++ party.py
@@ -0,0 +1,10 @@
+if today == 'Saturday':
+   print("Party")
+elif today == 'Sunday':
+   if condition == 'Headache':
+        print('Recover,then rest.')
+   else:
+        print('Rest')
+else:
+    print('Work,Work,Work.')
+    
